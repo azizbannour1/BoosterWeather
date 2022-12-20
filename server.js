fed3c5604ec00e32f3cafc433f379956e809184e
Mohamed Amine Bellili <73409316+medAmineBell@users.noreply.github.com>
2022-12-20 11:33:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ db.sequelize.sync()
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to Momo application." });
 });
 
 require("./app/routes/residance.routes")(app);
