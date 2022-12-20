668f333324d9d067782950426fbdcb8fdd3dfb4a
Mohamed Amine Bellili <73409316+medAmineBell@users.noreply.github.com>
2022-12-20 11:44:39 +0100

    Update residance.controller.js

--- app/controllers/residance.controller.js
+++ app/controllers/residance.controller.js
@@ -93,7 +93,7 @@ exports.update = (req, res) => {
     });
 };
 
-// Delete a Tutorial with the specified id in the request
+// Delete a Residance with the specified id in the request
 exports.delete = (req, res) => {
   const id = req.params.id;
 
